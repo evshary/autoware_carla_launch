60e5e7ae6c978f313ede077fc9abf7b3479749f8
ChenYing Kuo <evshary@gmail.com>
2023-02-23 17:26:36 +0800

    Able to limit ROS2 topic for zenoh-bridge-dds

--- env.sh
+++ env.sh
@@ -2,5 +2,8 @@ shell=`echo $SHELL | awk -F '/' '{print $NF}'`
 ENV_PATH=`realpath $0`
 export AUTOWARE_CARLA_ROOT=`dirname ${ENV_PATH}`
 
+# Source Autoware workspace
 source ${AUTOWARE_CARLA_ROOT}/install/local_setup.${shell}
 
+# Export the config of zenoh-bridge-dds
+export ZENOH_BRIDGE_DDS_CONFIG=${AUTOWARE_CARLA_ROOT}/zenoh-bridge-dds-conf.json5
