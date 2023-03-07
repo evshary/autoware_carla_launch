cacafc4bbae61d1c6def1f2ab2d33e98f88df8bc
ChenYing Kuo <evshary@gmail.com>
2023-03-07 09:59:04 +0800

    Able to set simulator IP.

--- env.sh
+++ env.sh
@@ -7,3 +7,6 @@ source ${AUTOWARE_CARLA_ROOT}/install/setup.${shell}
 
 # Export the config of zenoh-bridge-dds
 export ZENOH_BRIDGE_DDS_CONFIG=${AUTOWARE_CARLA_ROOT}/zenoh-bridge-dds-conf.json5
+
+# Export Carla simulator IP
+export CARLA_SIMULATOR_IP=127.0.0.1
