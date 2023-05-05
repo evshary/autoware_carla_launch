bfc7a6037eed522795cbd51a871d6d73d7d521a4
ChenYing Kuo <evshary@gmail.com>
2023-05-05 14:31:49 +0800

    Don't use ROS to run zenoh_carla_bridge

--- env.sh
+++ env.sh
@@ -3,18 +3,33 @@ ENV_PATH=`realpath $0`
 export AUTOWARE_CARLA_ROOT=`dirname ${ENV_PATH}`
 
 
-# Source workspace
+# Setup environmental variables for different environments
 shell=`cat /proc/$$/cmdline | tr -d '\0' | tr -d '-'`
-if [ -d /opt/ros/humble/ ] && [ -f ${AUTOWARE_CARLA_ROOT}/install/setup.${shell} ]; then
+if [ -d /opt/ros/humble/ ] && [ -f ${AUTOWARE_CARLA_ROOT}/install/setup.${shell} ]; then  # zenoh-bridge-dds & Autoware
+
+    # Source workspace
     source ${AUTOWARE_CARLA_ROOT}/install/setup.${shell}
+
+    # Export the config of zenoh-bridge-dds
+    export ZENOH_BRIDGE_DDS_CONFIG=${AUTOWARE_CARLA_ROOT}/zenoh-bridge-dds-conf.json5
+
+else  # Python agent & zenoh_carla_bridge
+
+    # Export Carla simulator IP
+    export CARLA_SIMULATOR_IP=127.0.0.1
+
+    # Poetry path (Only needed while using docker)
+    if [ -f /.dockerenv ]; then
+        POETRY_PATH=${AUTOWARE_CARLA_ROOT}/poetry
+        PYENV_PATH=${AUTOWARE_CARLA_ROOT}/pyenv
+
+        export POETRY_HOME=${POETRY_PATH}
+        export PATH="${POETRY_PATH}/bin:$PATH"
+        export PYENV_ROOT="${PYENV_PATH}"
+        export PATH="${PYENV_ROOT}/bin:$PATH"
+    fi
+
 fi
-
-# Export the config of zenoh-bridge-dds
-export ZENOH_BRIDGE_DDS_CONFIG=${AUTOWARE_CARLA_ROOT}/zenoh-bridge-dds-conf.json5
-
-
-# Export Carla simulator IP
-export CARLA_SIMULATOR_IP=127.0.0.1
 
 
 # Export Map path
@@ -27,17 +42,11 @@ export ROS_DOMAIN_ID=0
 export VEHICLE_NAME="v1"
 
 
-# Rust & Poetry path (Only needed while using docker)
+# Rust path (Only needed while using docker)
 if [ -f /.dockerenv ]; then
     RUST_PATH=${AUTOWARE_CARLA_ROOT}/rust
-    POETRY_PATH=${AUTOWARE_CARLA_ROOT}/poetry
-    PYENV_PATH=${AUTOWARE_CARLA_ROOT}/pyenv
 
     export RUSTUP_HOME=${RUST_PATH}
     export CARGO_HOME=${RUST_PATH}
     export PATH="${RUST_PATH}/bin:$PATH"
-    export POETRY_HOME=${POETRY_PATH}	
-    export PATH="${POETRY_PATH}/bin:$PATH"
-    export PYENV_ROOT="${PYENV_PATH}"
-    export PATH="${PYENV_ROOT}/bin:$PATH"
 fi
---
+++ zenoh-carla-bridge.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+
+PYTHON_AGENT_PATH=${AUTOWARE_CARLA_ROOT}/external/zenoh_carla_bridge/carla_agent
+parallel --verbose ::: "RUST_LOG=z=info ${AUTOWARE_CARLA_ROOT}/external/zenoh_carla_bridge/target/release/zenoh_carla_bridge" \
+                       "poetry -C ${PYTHON_AGENT_PATH} run python3 ${PYTHON_AGENT_PATH}/main.py \
+                               --host ${CARLA_SIMULATOR_IP} --rolename ${VEHICLE_NAME}"
