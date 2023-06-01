67349ab03fa955ed648cef1f5b5d6c8248d2adfd
ChenYing Kuo <evshary@gmail.com>
2023-06-01 16:33:19 +0800

    Support using IP to connect Zenoh network.

--- script/run-autoware.sh
+++ script/run-autoware.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 VEHICLE_NAME="${1:-v1}"
+ZENOH_CARLA_IP="${2:-127.0.0.1}"
 
-VEHICLE_NAME=${VEHICLE_NAME} ros2 launch autoware_carla_launch autoware_zenoh.launch.xml
-
+VEHICLE_NAME=${VEHICLE_NAME} ZENOH_CARLA_IP=${ZENOH_CARLA_IP} ros2 launch autoware_carla_launch autoware_zenoh.launch.xml
--- script/run-bridge-two-vehicles.sh
+++ script/run-bridge-two-vehicles.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 PYTHON_AGENT_PATH=${AUTOWARE_CARLA_ROOT}/external/zenoh_carla_bridge/carla_agent
-parallel --verbose ::: "RUST_LOG=z=info ${AUTOWARE_CARLA_ROOT}/external/zenoh_carla_bridge/target/release/zenoh_carla_bridge" \
+parallel --verbose ::: "RUST_LOG=z=info ${AUTOWARE_CARLA_ROOT}/external/zenoh_carla_bridge/target/release/zenoh_carla_bridge \
+                               --zenoh-listen tcp/0.0.0.0:7447" \
                        "poetry -C ${PYTHON_AGENT_PATH} run python3 ${PYTHON_AGENT_PATH}/main.py \
                                --host ${CARLA_SIMULATOR_IP} --rolename 'v1' \
                                --position 87.687683,145.671295,0.300000,0.000000,90.000053,0.000000" \
