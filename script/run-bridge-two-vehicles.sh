5151ba7347c3af7f12e9859ff407939455c58253
ChenYing Kuo <evshary@gmail.com>
2023-07-17 10:28:45 +0800

    Delay the bridge to make sure Carla setting runs as we expect.
    I think the reason why we can't press auto is from this issue.

--- script/run-bridge-two-vehicles.sh
+++ script/run-bridge-two-vehicles.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 PYTHON_AGENT_PATH=${AUTOWARE_CARLA_ROOT}/external/zenoh_carla_bridge/carla_agent
+# Note bridge should run later because it needs to configure Carla sync setting.
+# Python script will overwrite the settings if bridge run first.
 parallel --verbose --lb --header :  --results bridge_log ::: "`date '+%Y-%m-%d_%H:%M:%S'`" \
-                       "RUST_LOG=z=info ${AUTOWARE_CARLA_ROOT}/external/zenoh_carla_bridge/target/release/zenoh_carla_bridge --zenoh-listen tcp/0.0.0.0:7447" \
+                       "sleep 5 && RUST_LOG=z=info ${AUTOWARE_CARLA_ROOT}/external/zenoh_carla_bridge/target/release/zenoh_carla_bridge --zenoh-listen tcp/0.0.0.0:7447" \
                        "poetry -C ${PYTHON_AGENT_PATH} run python3 ${PYTHON_AGENT_PATH}/main.py \
                                --host ${CARLA_SIMULATOR_IP} --rolename 'v1' \
                                --position 87.687683,145.671295,0.300000,0.000000,90.000053,0.000000" \
