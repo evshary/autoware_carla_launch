2c0e11f35e3c52902d1dba6dc4ee0093fef2e3e7
ChenYing Kuo <evshary@gmail.com>
2023-10-25 16:11:35 +0800

    Able to assign FMS IP & port

--- script/run-autoware.sh
+++ script/run-autoware.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 VEHICLE_NAME="${1:-v1}"
-ZENOH_CARLA_IP="${2:-127.0.0.1}"
+ZENOH_CARLA_IP_PORT="${2:-'127.0.0.1:7447'}"
+ZENOH_FMS_IP_PORT="${3:-'127.0.0.1:7887'}"
 
-VEHICLE_NAME=${VEHICLE_NAME} ZENOH_CARLA_IP=${ZENOH_CARLA_IP} ros2 launch autoware_carla_launch autoware_zenoh.launch.xml
+VEHICLE_NAME=${VEHICLE_NAME} ZENOH_CARLA_IP_PORT=${ZENOH_CARLA_IP_PORT} ZENOH_FMS_IP_PORT=${ZENOH_FMS_IP_PORT} ros2 launch autoware_carla_launch autoware_zenoh.launch.xml
