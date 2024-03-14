f6bbf19cc4bb3a40a66d36c8d1040002cce40df1
RogerJung <engineer54rj@gmail.com>
2024-03-14 23:57:22 +0800

    Update carla .osm map that has traffic lights' information.

--- script/download_map.sh
+++ script/download_map.sh
@@ -9,7 +9,7 @@ python3 -m pip install --upgrade gdown
 if [ ! -d $MAP_PATH ]; then
     echo "Download map from Internet..."
     mkdir $MAP_PATH
-    gdown --fuzzy -O $MAP_PATH/lanelet2_map.osm https://drive.google.com/file/d/1vm9SvalJe7Bc9sh_8jK-0cPulVAov5uD/view
+    gdown --fuzzy -O $MAP_PATH/lanelet2_map.osm https://drive.google.com/file/d/1TBfWKDxxGnfm1ZUfzotDgcadEERYr85s/view
     gdown --fuzzy -O $MAP_PATH/pointcloud_map.pcd https://drive.google.com/file/d/1MvJlfjkw3LWFUs5hdE_KQ2_CQTusU8UN/view
     echo "Download complete"
 fi
