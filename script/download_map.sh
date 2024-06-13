2ee1c00beea6b2cc435e931719b6f6a2b52bc6f9
hsule <leann9001@gmail.com>
2024-06-13 10:28:23 +0800

    fix: map download problem

--- script/download_map.sh
+++ script/download_map.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
 
 MAP_PATH="carla_map/Town01"
 
@@ -7,8 +8,11 @@ python3 -m pip install --upgrade gdown
 
 # If map doesn't exist
 if [ ! -d $MAP_PATH ]; then
-    echo "Download map from Internet..."
     mkdir $MAP_PATH
+fi
+
+if [ ! -f "$MAP_PATH/lanelet2_map.osm" ] || [ ! -f "$MAP_PATH/pointcloud_map.pcd" ]; then
+    echo "Download map from Internet..."
     gdown --fuzzy -O $MAP_PATH/lanelet2_map.osm https://drive.google.com/file/d/1vm9SvalJe7Bc9sh_8jK-0cPulVAov5uD/view
     gdown --fuzzy -O $MAP_PATH/pointcloud_map.pcd https://drive.google.com/file/d/1MvJlfjkw3LWFUs5hdE_KQ2_CQTusU8UN/view
     echo "Download complete"
