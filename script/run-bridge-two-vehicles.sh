378e68fc41c281b58ecab17f97c6b919948b866a
MamoruDS <42025957+MamoruDS@users.noreply.github.com>
2024-07-05 11:33:44 +0900

    fix: add missing CARLA simulator port arg to bridge start cmd (#111)
    The environment variable for the CARLA simulator port has been defined in `env.sh` but hasn't been utilized in the bridge start command.
    This commit ensures it is now used correctly.

--- script/run-bridge-two-vehicles.sh
+++ script/run-bridge-two-vehicles.sh
@@ -10,7 +10,7 @@ mkdir -p ${LOG_PATH}
 # Python script will overwrite the settings if bridge run first.
 parallel --verbose --lb ::: \
         "sleep 5 && RUST_LOG=z=info ${AUTOWARE_CARLA_ROOT}/external/zenoh_carla_bridge/target/release/zenoh_carla_bridge \
-                --mode ros2 --zenoh-listen tcp/0.0.0.0:7447 --slowdown 2 2>&1 | tee ${LOG_PATH}/bridge.log" \
+                --mode ros2 --zenoh-listen tcp/0.0.0.0:7447 --carla-address ${CARLA_SIMULATOR_IP} --slowdown 2 2>&1 | tee ${LOG_PATH}/bridge.log" \
         "poetry -C ${PYTHON_AGENT_PATH} run python3 ${PYTHON_AGENT_PATH}/main.py \
                 --host ${CARLA_SIMULATOR_IP} --rolename 'v1' \
                 --position 87.687683,145.671295,0.300000,0.000000,90.000053,0.000000 \
