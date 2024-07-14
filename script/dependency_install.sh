bf4d594c36fc4fe812ce3ea253061fa9f070d747
habby1012 <80194639+habby1012@users.noreply.github.com>
2024-07-14 15:18:55 +0800

    small bug fix (#118)
    * small bug fix
    
    * Update dependency_install.sh

--- script/dependency_install.sh
+++ script/dependency_install.sh
@@ -15,8 +15,8 @@ function install_python()
     curl -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
     pyenv install -v 3.8.10
     pyenv global 3.8.10
-    cd external/zenoh_carla_bridge/carla_agent && poetry env use $(pyenv which python)
-    cd external/zenoh_autoware_v2x && poetry env use $(pyenv which python) && poetry install --no-root
+    cd ${AUTOWARE_CARLA_ROOT}/external/zenoh_carla_bridge/carla_agent && poetry env use $(pyenv which python)
+    cd ${AUTOWARE_CARLA_ROOT}/external/zenoh_autoware_v2x && poetry env use $(pyenv which python) && poetry install --no-root
 }
 
 function install_rust()
