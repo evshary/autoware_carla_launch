90e0098e8382532c214b45e3a030d8c8f9402fbc
ChenYing Kuo (CY) <evshary@gmail.com>
2024-10-16 14:58:52 +0800

    Build docker images without cache. (#131)
    Signed-off-by: ChenYing Kuo <evshary@gmail.com>

--- container/run-bridge-docker.sh
+++ container/run-bridge-docker.sh
@@ -5,7 +5,7 @@ DOCKER_FILE=container/Dockerfile_carla_bridge
 
 if [ ! "$(docker images -q ${DOCKER_IMAGE})" ]; then
     echo "${DOCKER_IMAGE} does not exist. Creating..."
-    docker build -f ${DOCKER_FILE} -t ${DOCKER_IMAGE} .
+    docker build --no-cache -f ${DOCKER_FILE} -t ${DOCKER_IMAGE} .
 fi
 
 rocker --nvidia --network host --privileged --x11 --user --volume $(pwd):$HOME/autoware_carla_launch -- ${DOCKER_IMAGE}
