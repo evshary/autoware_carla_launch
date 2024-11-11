e13a3ee7ede1357e24870d7a5a61fa7103af468a
habby <a0979580915@gmail.com>
2024-11-11 12:28:01 +0800

    use new map which has traffic_light information

--- script/download_map.sh
+++ script/download_map.sh
@@ -13,7 +13,7 @@ fi
 
 if [ ! -f "$MAP_PATH/lanelet2_map.osm" ] || [ ! -f "$MAP_PATH/pointcloud_map.pcd" ] || [ ! -f "$MAP_PATH/map_projector_info.yaml" ]; then
     echo "Download map from Internet..."
-    gdown --fuzzy -O $MAP_PATH/lanelet2_map.osm https://drive.google.com/file/d/1vm9SvalJe7Bc9sh_8jK-0cPulVAov5uD/view
+    gdown --fuzzy -O $MAP_PATH/lanelet2_map.osm https://drive.google.com/file/d/1CMfWin62qIEqQbcIQ6UfplztZDjMBKD6/view
     gdown --fuzzy -O $MAP_PATH/pointcloud_map.pcd https://drive.google.com/file/d/1MvJlfjkw3LWFUs5hdE_KQ2_CQTusU8UN/view
     gdown --fuzzy -O $MAP_PATH/map_projector_info.yaml https://drive.google.com/file/d/1h1me3TXSoTBL86S94QKlkwSpwUeZXv2J/view
     echo "Download complete"
