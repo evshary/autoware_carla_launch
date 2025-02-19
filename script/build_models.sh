03ce8a596d9e3f1991a4e63c2b016e16fdc13b23
habby1012 <80194639+habby1012@users.noreply.github.com>
2025-02-19 13:38:54 +0800

    Enable Traffic light module  (#154)
    * download traffic light models
    
    * config camera position and angle
    
    * use traffic light module
    
    * prebuild traffic light classifier models
    
    * add build_only when prebuilding traffic light classifier models
    
    * modify env.sh - Use correct traffic light topic for traffic light recognition
    
    * Add a script, which runs traffic light module
    
    * Add some parameters for traffic light module
    
    * download traffic light map
    
    * zenoh carla bridge new commit
    
    * use new zenoh carla bridge submodule
    
    * Add comment in traffic light launch script
    
    * Create autoware_traffic_light.rst

--- script/build_models.sh
+++ script/build_models.sh
@@ -23,3 +23,19 @@ ros2 launch autoware_lidar_transfusion lidar_transfusion.launch.xml \
         model_path:=$AUTOWARE_CARLA_ROOT/autoware_data/lidar_transfusion \
         model_param_path:=$(ros2 pkg prefix autoware_launch --share)/config/perception/object_recognition/detection/lidar_model/transfusion.param.yaml \
         build_only:=true
+
+# traffic light (car)
+ros2 launch autoware_traffic_light_classifier traffic_light_classifier.launch.xml \
+	car_traffic_light_classifier_label_name:=traffic_light_classifier_mobilenetv2_batch_6 \
+	car_traffic_light_classifier_model_name:=traffic_light_classifier_mobilenetv2_batch_6 \
+	traffic_light_classifier_model_path:=$AUTOWARE_CARLA_ROOT/autoware_data/traffic_light_classifier \
+	traffic_light_classifier_param_path:=$(ros2 pkg prefix autoware_traffic_light_classifier --share)/config/car_traffic_light_classifier.param.yaml \
+	build_only:=true
+
+# traffic light (pedestrian)
+ros2 launch autoware_traffic_light_classifier traffic_light_classifier.launch.xml \
+	pedestrian_traffic_light_classifier_label_name:=ped_traffic_light_classifier_mobilenetv2_batch_4 \
+	pedestrian_traffic_light_classifier_model_name:=ped_traffic_light_classifier_mobilenetv2_batch_4 \
+	traffic_light_classifier_model_path:=$AUTOWARE_CARLA_ROOT/autoware_data/traffic_light_classifier \
+	traffic_light_classifier_param_path:=$(ros2 pkg prefix autoware_traffic_light_classifier --share)/config/pedestrian_traffic_light_classifier.param.yaml \
+	build_only:=true
---
+++ script/run-autoware-traffic-light.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -e
+
+export VEHICLE_NAME="${1:-v1}"
+export ZENOH_CARLA_IP_PORT="${2:-'127.0.0.1:7447'}"
+export ZENOH_FMS_IP_PORT="${3:-'127.0.0.1:7887'}"
+
+# Add default port for Zenoh IP in Carla and FMS
+if [[ ${ZENOH_CARLA_IP_PORT} != *":"* ]]; then
+    export ZENOH_CARLA_IP_PORT="${ZENOH_CARLA_IP_PORT}:7447"
+fi
+if [[ ${ZENOH_FMS_IP_PORT} != *":"* ]]; then
+    export ZENOH_FMS_IP_PORT="${ZENOH_FMS_IP_PORT}:7887"
+fi
+
+# Log folder
+LOG_PATH=autoware_log/`date '+%Y-%m-%d_%H:%M:%S'`/
+mkdir -p ${LOG_PATH}
+
+# Run the program
+# If is_simulation is true, planning behavior will change and doesn't care about the traffic light recognition. So we make it false.
+parallel --verbose --lb ::: \
+    "ros2 launch autoware_carla_launch autoware_zenoh.launch.xml is_simulation:=false use_traffic_light_recognition:=true \
+            2>&1 | tee ${LOG_PATH}/autoware.log" \
+    "${AUTOWARE_CARLA_ROOT}/external/zenoh-plugin-ros2dds/target/release/zenoh-bridge-ros2dds \
+            -n /${VEHICLE_NAME} -d ${ROS_DOMAIN_ID} -c ${ZENOH_BRIDGE_ROS2DDS_CONFIG} -e tcp/${ZENOH_CARLA_IP_PORT} -e tcp/${ZENOH_FMS_IP_PORT} \
+            2>&1 | tee ${LOG_PATH}/zenoh_bridge_ros2dds.log"
