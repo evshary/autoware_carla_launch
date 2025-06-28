bde706c12850285190b5b8c783f177d966801cc6
habby <a0979580915@gmail.com>
2025-06-28 20:45:31 +0800

    Install and build rmw_zenoh

---
+++ script/setup/build_zenoh.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ ! -d "rmw_zenoh_ws" ]; then
+    mkdir rmw_zenoh_ws/src -p
+    cd rmw_zenoh_ws/src || exit
+    git clone https://github.com/ros2/rmw_zenoh.git -b humble
+    cd rmw_zenoh || exit
+    git checkout 65ded05
+    cd ../.. || exit
+    rosdep install --from-paths src --ignore-src --rosdistro humble -y
+    cd .. || exit
+fi
+
+cd rmw_zenoh_ws || exit
+source /opt/ros/humble/setup.bash
+colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release
