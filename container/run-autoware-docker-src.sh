9c8f3d80c858e666299e9215e10262a41c80b897
Leann Hsu <leann9001@gmail.com>
2025-08-29 13:57:17 +0800

    Update docker to 0814 & remove autoware_launch

--- container/run-autoware-docker-src.sh
+++ container/run-autoware-docker-src.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-DOCKER_IMAGE=zenoh-autoware-20250807
+DOCKER_IMAGE=zenoh-autoware-20250814
 DOCKER_FILE=container/Dockerfile_autoware
 AUTOWARE_VERSION=main
-AUTOWARE_COMMIT=afb3dff99a781f91b4029fbb8835e338f50ecc6a #20250808
+AUTOWARE_COMMIT=447f0e1598c66b53c4445428de1857cc607e8a34 #20250814
 
 if [ ! "$(docker images -q ${DOCKER_IMAGE})" ]; then
     echo "${DOCKER_IMAGE} does not exist. Creating..."
