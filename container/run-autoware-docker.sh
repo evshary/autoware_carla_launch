36d55c48687d66acea05294575b1cf7ecb4d6968
habby <a0979580915@gmail.com>
2025-09-02 21:50:44 +0800

    Enable Zenoh shared memory && Filter ingress/egress topics in Zenoh router

--- container/run-autoware-docker.sh
+++ container/run-autoware-docker.sh
@@ -8,5 +8,5 @@ if [ ! "$(docker images -q ${DOCKER_IMAGE})" ]; then
     docker build --no-cache -f ${DOCKER_FILE} -t ${DOCKER_IMAGE} .
 fi
 
-rocker --nvidia --privileged --x11 --user --volume $(pwd):$HOME/autoware_carla_launch -- ${DOCKER_IMAGE}
+rocker --nvidia --privileged --x11 --user --ipc host  --volume $(pwd):$HOME/autoware_carla_launch -- ${DOCKER_IMAGE}
 
