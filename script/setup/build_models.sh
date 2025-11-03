cbe5cbf3825fc95b8ee63ac0db98383e7a705889
Leann Hsu <leann9001@gmail.com>
2025-11-03 16:14:15 +0800

    Fix traffic_light and rmw_zenoh configs

--- script/setup/build_models.sh
+++ script/setup/build_models.sh
@@ -24,16 +24,20 @@ ros2 launch autoware_lidar_transfusion lidar_transfusion.launch.xml \
         model_param_path:=$(ros2 pkg prefix autoware_launch --share)/config/perception/object_recognition/detection/lidar_model/transfusion.param.yaml \
         build_only:=true
 
+# traffic light fine detector
+ros2 launch autoware_traffic_light_fine_detector traffic_light_fine_detector.launch.xml \
+	model_path:=$AUTOWARE_CARLA_ROOT/autoware_data/traffic_light_fine_detector/tlr_car_ped_yolox_s_batch_6.onnx \
+	label_path:=$AUTOWARE_CARLA_ROOT/autoware_data/traffic_light_fine_detector/tlr_labels.txt \
+	build_only:=true
+
 # traffic light (car)
 ros2 launch autoware_traffic_light_classifier car_traffic_light_classifier.launch.xml \
-	param_path:=$(ros2 pkg prefix autoware_traffic_light_classifier --share)/config/car_traffic_light_classifier.param.yaml \
 	model_path:=$AUTOWARE_CARLA_ROOT/autoware_data/traffic_light_classifier/traffic_light_classifier_mobilenetv2_batch_6.onnx \
 	label_path:=$AUTOWARE_CARLA_ROOT/autoware_data/traffic_light_classifier/lamp_labels.txt \
 	build_only:=true \
 
 # traffic light (pedestrian)
 ros2 launch autoware_traffic_light_classifier pedestrian_traffic_light_classifier.launch.xml \
-	param_path:=$(ros2 pkg prefix autoware_traffic_light_classifier --share)/config/pedestrian_traffic_light_classifier.param.yaml \
 	model_path:=$AUTOWARE_CARLA_ROOT/autoware_data/traffic_light_classifier/ped_traffic_light_classifier_mobilenetv2_batch_6.onnx \
 	label_path:=$AUTOWARE_CARLA_ROOT/autoware_data/traffic_light_classifier/lamp_labels_ped.txt \
 	build_only:=true \
