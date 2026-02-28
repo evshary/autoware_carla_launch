46f4ff56930daa9e75102d6337306193770d7728
Leann Hsu <leann9001@gmail.com>
2026-02-28 16:15:22 +0800

    Support rmw_zenoh in jazzy

--- script/autoware_rmw_zenoh/run-autoware-with-rmw_zenoh.sh
+++ script/autoware_rmw_zenoh/run-autoware-with-rmw_zenoh.sh
@@ -12,6 +12,8 @@ elif [[ "$VEHICLE_NAME" == "v2" ]]; then
 fi
 export ZENOH_SESSION_CONFIG_URI=config/RMW_ZENOH_SESSION_CONFIG.json5
 
+# Set namespace to match bridge key expressions
+export ZENOH_CONFIG_OVERRIDE="namespace=\"${VEHICLE_NAME}\""
 # Enable Zenoh shared memory
 # export ZENOH_CONFIG_OVERRIDE="namespace=\"${VEHICLE_NAME}\";transport/shared_memory/enabled=true"
 # export ZENOH_SHM_ALLOC_SIZE=$((256 * 1024 * 1024))
@@ -21,27 +23,14 @@ export ZENOH_SESSION_CONFIG_URI=config/RMW_ZENOH_SESSION_CONFIG.json5
 LOG_PATH=autoware_log/`date '+%Y-%m-%d_%H:%M:%S'`/
 mkdir -p ${LOG_PATH}
 
-# Overwrite the default behavior_planning.launch.xml with the single-threaded version
-# It runs behavior_path_planner in a separate container with one thread
-# to avoid a GuardCondition use-after-free issue when using rmw_zenoh with multi-threaded executor.
-# Known issue in ROS 2 Humble; fixed in Rolling (2025.04+), not backported.
-if [[ "$AMENT_PREFIX_PATH" == *"/autoware/install"* ]]; then
-    DEST_PATH="autoware/src/universe/autoware_universe/launch"
-    SUDO=""
-else
-    DEST_PATH="/opt/autoware/share"
-    SUDO="sudo"
-fi
-$SUDO cp "$AUTOWARE_CARLA_ROOT/script/replace/behavior_planning_singlethread.launch.xml" "$DEST_PATH/tier4_planning_launch/launch/scenario_planning/lane_driving/behavior_planning/behavior_planning.launch.xml"
-
 # Run the program
 parallel --verbose --lb ::: \
     "ros2 launch autoware_carla_launch autoware_zenoh.launch.xml \
             use_traffic_light_recognition:=false \
             lidar_detection_model:=${LIDAR_DETECTION_MODEL}/${CENTERPOINT_MODEL_NAME} \
             traffic_light_recognition/camera_namespaces:=[traffic_light] \
-            input/pointcloud:="/sensing/lidar/top/pointcloud" \
-            input_pointcloud:="/sensing/lidar/top/pointcloud" \
+            input/pointcloud:="/sensing/lidar/top/pointcloud_raw_ex" \
+            input_pointcloud:="/sensing/lidar/top/pointcloud_raw_ex" \
             2>&1 | tee ${LOG_PATH}/autoware.log" \
     "RUST_LOG=debug ros2 run rmw_zenoh_cpp rmw_zenohd \
     	    2>&1 | tee ${LOG_PATH}/rmw_zenohd.log"
--- script/setup/build_zenoh.sh
+++ script/setup/build_zenoh.sh
@@ -3,7 +3,7 @@
 if [ ! -d "rmw_zenoh_ws" ]; then
     mkdir rmw_zenoh_ws/src -p
     cd rmw_zenoh_ws/src || exit
-    git clone https://github.com/ros2/rmw_zenoh.git -b humble
+    git clone https://github.com/ros2/rmw_zenoh.git -b jazzy
     cd .. || exit
     rosdep update
     rosdep install --from-paths src --ignore-src --rosdistro humble -y
@@ -11,5 +11,5 @@ if [ ! -d "rmw_zenoh_ws" ]; then
 fi
 
 cd rmw_zenoh_ws || exit
-source /opt/ros/humble/setup.bash
+source /opt/ros/jazzy/setup.bash
 colcon build --cmake-args -DCMAKE_BUILD_TYPE=Release
