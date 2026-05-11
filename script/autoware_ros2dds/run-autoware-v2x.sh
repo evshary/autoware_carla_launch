2794bc06fa7e12347853f9cab868cb08e981cefe
Leann Hsu <leann9001@gmail.com>
2026-05-11 13:35:59 +0800

    Add combined run-autoware-v2x scripts for V2X scenarios
    Signed-off-by: Leann Hsu <leann9001@gmail.com>

---
+++ script/autoware_ros2dds/run-autoware-v2x.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+set -e
+
+export VEHICLE_NAME="${1:-v1}"
+export ZENOH_CARLA_IP_PORT="${2:-'127.0.0.1:7447'}"
+export ZENOH_FMS_IP_PORT="${3:-'127.0.0.1:7887'}"
+
+# Add default port for Zenoh IP in Carla and FMS
+if [[ ${ZENOH_CARLA_IP_PORT} != *":"* ]]; then
+    export ZENOH_CARLA_IP_PORT="${ZENOH_CARLA_IP_PORT}:7447"
+fi
+if [[ ${ZENOH_FMS_IP_PORT} != *":"* ]]; then
+    export ZENOH_FMS_IP_PORT="${ZENOH_FMS_IP_PORT}:7887"
+fi
+
+# Source v2x_light overlay so ros2 run can find it in the parallel job below
+source external/zenoh_autoware_v2x/install/setup.bash
+
+# Log folder
+LOG_PATH=autoware_log/`date '+%Y-%m-%d_%H:%M:%S'`/
+mkdir -p ${LOG_PATH}
+
+# Run the program
+parallel --verbose --lb ::: \
+    "ros2 launch autoware_carla_launch autoware_zenoh.launch.xml \
+            2>&1 | tee ${LOG_PATH}/autoware.log" \
+    "${AUTOWARE_CARLA_ROOT}/external/zenoh-plugin-ros2dds/target/release/zenoh-bridge-ros2dds \
+            -n /${VEHICLE_NAME} -d ${ROS_DOMAIN_ID} -c ${ZENOH_BRIDGE_ROS2DDS_CONFIG} -e tcp/${ZENOH_CARLA_IP_PORT} -e tcp/${ZENOH_FMS_IP_PORT} \
+            2>&1 | tee ${LOG_PATH}/zenoh_bridge_ros2dds.log" \
+    "sleep 5 && ros2 run v2x_light v2x_light -- -v ${VEHICLE_NAME} --map-info external/zenoh_autoware_v2x/map_info.json \
+            2>&1 | tee ${LOG_PATH}/v2x_light.log"
